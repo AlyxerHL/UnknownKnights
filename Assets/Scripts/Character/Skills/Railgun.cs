ff3cccdc7ccb9fd26ea10ec6bacb4bd0080747c4
AlyxerHL <alyxer@alyxer.me>
2023-10-28 19:11:14 +0900

    Implement sojourn

---
+++ Assets/Scripts/Character/Skills/Railgun.cs
@@ -0,0 +1,23 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class Railgun : Skill
+{
+    private const float Damage = 135f;
+    private const float Range = 40f;
+    private const int RecoveryTime = 650;
+
+    [SerializeField]
+    private NearestCharacterFinder finder;
+
+    protected override bool CanUse =>
+        finder.Tag != null
+        && (transform.position - finder.Tag.transform.position).sqrMagnitude <= Range;
+
+    protected override async UniTask Use(CancellationToken cancellationToken)
+    {
+        finder.Tag.Health.GetDamaged(Damage);
+        await UniTask.Delay(RecoveryTime, cancellationToken: cancellationToken);
+    }
+}
