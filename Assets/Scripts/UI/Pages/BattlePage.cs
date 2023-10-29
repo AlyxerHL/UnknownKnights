41655aac0bdfdaba51d345bd59ed19f1124d3477
AlyxerHL <alyxer@alyxer.me>
2023-10-29 14:30:59 +0900

    Bind buttons

--- Assets/Scripts/UI/Pages/BattlePage.cs
+++ Assets/Scripts/UI/Pages/BattlePage.cs
@@ -1,6 +1,20 @@
 using Slash.Unity.DataBind.Core.Data;
+using UnityEngine;
 
 public class BattlePage : Context
 {
-    //
+    public void Pause()
+    {
+        Debug.Log("Pause");
+    }
+
+    public void ToggleAutoSkill()
+    {
+        Debug.Log("ToggleAutoSkill");
+    }
+
+    public void ToggleGameSpeed()
+    {
+        Debug.Log("ToggleGameSpeed");
+    }
 }
