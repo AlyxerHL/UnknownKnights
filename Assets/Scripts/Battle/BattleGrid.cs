9db0c1d6ebeca7ea479ab6a0163eebfd4fbc7f0a
AlyxerHL <alyxer@alyxer.me>
2023-11-02 21:30:47 +0900

    Change default spawn position

--- Assets/Scripts/Battle/BattleGrid.cs
+++ Assets/Scripts/Battle/BattleGrid.cs
@@ -12,7 +12,7 @@ public class BattleGrid : ScriptableObject
     [field: SerializeField]
     public Character Back { get; set; }
 
-    public static readonly Vector2 TopPosition = new(-6f, 1f);
-    public static readonly Vector2 BottomPosition = new(-6f, -1f);
-    public static readonly Vector2 BackPosition = new(-8f, 0f);
+    public static readonly Vector2 TopPosition = new(-6f, 0.5f);
+    public static readonly Vector2 BottomPosition = new(-6f, -1.5f);
+    public static readonly Vector2 BackPosition = new(-8f, -0.5f);
 }
