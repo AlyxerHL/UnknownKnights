f0345a77b55dd0b9a6dd901cf3514b318cd4e6e5
AlyxerHL <alyxer@alyxer.me>
2023-11-02 19:00:23 +0900

    Add home screen

---
+++ Assets/Scripts/UI/Pages/HomePage/HomePage.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomePage : MonoBehaviour
+{
+    public void StartBattle()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
